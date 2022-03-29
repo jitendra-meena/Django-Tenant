4a300d8ed9da4e3d59d79c7ac19683f3731edf8d
Jitendra Meena <jitendrameena9575@gmail.com>
2022-03-29 23:11:18 +0530

    DB update

--- DataBaseTenant/settings.py
+++ DataBaseTenant/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'tenant_schemas.middleware.TenantMiddleware',
     'django_tenants.middleware.main.TenantMainMiddleware', # Changes 
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
