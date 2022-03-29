bb6dd97a0e40dbe1bcc2fe380c1989713fa9ef23
Jitendra Meena <jitendrameena9575@gmail.com>
2022-03-29 23:19:15 +0530

    Client

--- Tenant/models.py
+++ Tenant/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.db import models
+from tenant_schemas.models import TenantMixin
 
-# Create your models here.
+class Client(TenantMixin):
+    name = models.CharField(max_length=100)
+    paid_until =  models.DateField()
+    on_trial = models.BooleanField()
+    created_on = models.DateField(auto_now_add=True)
